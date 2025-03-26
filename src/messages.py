980bddc01d1a1ca597616722e8da7568ad5d62b9
ltrias <lucas.trias@gmail.com>
2025-03-26 15:09:10 -0300

    Moving messages to external module

---
+++ src/messages.py
@@ -0,0 +1,20 @@
+import os
+
+
+def print_usage():
+    message = """
+    Usage: python +script_name+  <src filename> <dst filename> <metric>
+        <src filename>: origin of the <metric> data
+        <dst filename>: file where <src filename> <metric> will be merged into
+        <metric>: metric to migrate, defaults to HR (heart rate)
+    """
+
+    print(message.replace("+script_name+", os.path.basename(__file__)))
+
+
+def print_header():
+    message = """TCX Data Migrator by Lucas Trias - github.com/ltrias
+    Allows to merge data from a tcx file into another one
+    For instance, merge Polar chest strap heart rate data into Garmin watch tracked activity"""
+
+    print(message)
--- src/tcx-data-migrator.py
+++ src/tcx-data-migrator.py
@@ -1,5 +1,5 @@
 import sys
-import os
+import messages
 
 
 def valid_parameters(param):
@@ -11,30 +11,11 @@ def valid_parameters(param):
         return result
 
 
-def print_usage():
-    message = """
-    Usage: python +script_name+  <src filename> <dst filename> <metric>
-        <src filename>: origin of the <metric> data
-        <dst filename>: file where <src filename> <metric> will be merged into
-        <metric>: metric to migrate, defaults to HR (heart rate)
-    """
-
-    print(message.replace("+script_name+", os.path.basename(__file__)))
-
-
-def print_header():
-    message = """TCX Data Migrator by Lucas Trias - github.com/ltrias
-    Allows to merge data from a tcx file into another one
-    For instance, merge Polar chest strap heart rate data into Garmin watch tracked activity"""
-
-    print(message)
-
-
 def main():
-    print_header()
+    messages.print_header()
 
     if not valid_parameters(sys.argv):
-        print_usage()
+        messages.print_usage()
         return
 
 
