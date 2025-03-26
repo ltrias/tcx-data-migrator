f1dc3b02825cb564cec5a3db8f2b5924955a83a5
ltrias <lucas.trias@gmail.com>
2025-03-26 17:12:56 -0300

    Adding pretty print option #3

--- src/TCXFile.py
+++ src/TCXFile.py
@@ -8,17 +8,20 @@ TCX_NAMESPACE = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
 class TCXFile:
     def __init__(self, filename):
         ET.register_namespace('', TCX_NAMESPACE)
+
         try:
             self.filename = filename
             self.tree = ET.parse(self.filename)
         except:
             print("Cannot load or parse " + self.filename)
 
-    def save(self, filename=None):
+    def save(self, filename=None, pretty_print=True):
         if filename is None:
             filename = self.filename
         
         try:
+            if pretty_print:
+                ET.indent(self.tree)
             self.tree.write(filename, encoding="UTF-8", xml_declaration=True)
             print("Saving " + filename)
         except:
