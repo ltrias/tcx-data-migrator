2a3ffb8570442b93f8c48ff50e7ba3667d3c98b4
Lucas Trias <lucas.trias@gmail.com>
2025-06-04 12:32:18 -0300

    Defining expected behavior

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -6,25 +6,31 @@ from src import strategies
 class TestTrackpointIndexStrategy(unittest.TestCase):
     
     def setUp(self):
-        self._bigTCXData = [*range(100)]
+        self.subject = strategies.TrackpointIndexStrategy()
+        
+        self.bigTCXData = [*range(100)]
         small_hr_data = [*range(10)]
-        self._smallTCXData = small_hr_data
-        self._revSmallTCXData = [*reversed(small_hr_data)]
+        self.smallTCXData = small_hr_data
+        self.revSmallTCXData = [*reversed(small_hr_data)]
     
     @unittest.skip("Actually a DataMigationContext test")
     def testDefaultArguments(self):
-        self.fail()
         subject = strategies.DataMigrationContext(None, None) 
         self.assertEqual('HR', subject._metric)
         self.assertIsInstance(subject._strategy, strategies.TrackpointIndexStrategy)
 
     def test_same_number_of_samples(self):
-        subject = strategies.TrackpointIndexStrategy()
-        subject.migrate(self._smallTCXData, self._revSmallTCXData)
-        self.assertEqual(self._smallTCXData, self._revSmallTCXData)
+        self.subject.migrate(self.smallTCXData, self.revSmallTCXData)
+        self.assertEqual(self.smallTCXData, self.revSmallTCXData)
 
     def test_src_has_more_samples(self):
-        self.fail()
+        size_before = len(self.revSmallTCXData)
+        self.subject.migrate(self.bigTCXData, self.revSmallTCXData)
+        self.assertEqual(self.bigTCXData[:len(self.revSmallTCXData)], self.revSmallTCXData)
+        self.assertEqual(size_before, len(self.revSmallTCXData))
 
     def test_src_has_less_samples(self):
-        self.fail()
+        size_before = len(self.bigTCXData)
+        self.subject.migrate(self.revSmallTCXData, self.bigTCXData)
+        self.assertEqual(self.revSmallTCXData, self.bigTCXData[:len(self.revSmallTCXData)])
+        self.assertEqual(size_before, len(self.bigTCXData))
