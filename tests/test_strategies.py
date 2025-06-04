4480549bf517cd491f6d583cd6a21c89eda0c5ad
Lucas Trias <lucas.trias@gmail.com>
2025-06-04 11:53:58 -0300

    Refactoring TrackpointIndexStrategy tests

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,20 +1,27 @@
 
 import unittest
+from unittest.mock import MagicMock
 from src import strategies
 
 class TestTrackpointIndexStrategy(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        pass
-
-
+    
+    def setUp(self):
+        self._bigTCXData = [*range(100)]
+        small_hr_data = [*range(10)]
+        self._smallTCXData = small_hr_data
+        self._revSmallTCXData = [*reversed(small_hr_data)]
+    
+    @unittest.skip("Actually a DataMigationContext test")
     def testDefaultArguments(self):
+        self.fail()
         subject = strategies.DataMigrationContext(None, None) 
         self.assertEqual('HR', subject._metric)
         self.assertIsInstance(subject._strategy, strategies.TrackpointIndexStrategy)
 
     def test_same_number_of_samples(self):
-        self.fail()
+        subject = strategies.TrackpointIndexStrategy()
+        subject.migrate(self._smallTCXData, self._revSmallTCXData)
+        self.assertEqual(self._smallTCXData, self._revSmallTCXData)
 
     def test_src_has_more_samples(self):
         self.fail()
